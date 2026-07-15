59847e2fccac19f5a444d1e8a3725039d4d5ef60
Holger Seelig <holger.seelig@gmail.com>
2026-07-15 17:31:13 +0200

    Updated eslint config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,30 @@
-import js from "@eslint/js";
-import globals from "globals";
+import js               from "@eslint/js";
+import globals          from "globals";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+export default defineConfig ([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals:
+      {
+        ... globals .browser,
+        ... globals .node,
+      }
+    },
+    rules: {
+      "no-async-promise-executor": "off",
+      "no-empty": ["error", { "allowEmptyCatch": true }],
+      "semi": "error",
+    },
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions:
+    {
+      sourceType: "commonjs",
+    }
+  },
 ]);
